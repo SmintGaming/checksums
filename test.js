b80b2e8859c41149783368fcc82403cb2bad6827
SmintCodes <smintoverflow@gmail.com>
2020-05-01 15:23:15 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const { execSync } = require('child_process');
 // stderr is sent to stdout of parent process
 // you can set options.stdio if you want it to go elsewhere
-const stdout = execSync('./checksums.js');
+const stdout = execSync('node checksums.js');
 const { spawnSync } = require('child_process');
-const child = spawnSync('./checksums.js');
+const child = spawnSync('node checksums.js');
 const sstdout = stdout.toString('utf8')
 /*console.error('error', child.error);*/
 console.log(sstdout);
